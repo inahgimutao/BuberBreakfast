2dd30e1f6358921a113d7fd4ab0bd8d0b91b354d
regina gimutao <gimutaoregina191@gmail.com>
2023-09-13 09:00:38 +0800

    buber breakfast

---
+++ BuberBreakfast/ServiceErrors/Errors.Breakfast.cs
@@ -0,0 +1,24 @@
+using ErrorOr;
+
+namespace BuberBreakfast.ServiceErrors;
+
+public static class Errors
+{
+    public static class Breakfast
+    {
+        public static Error InvalidName => Error.Validation(
+            code: "Breakfast.InvalidName",
+            description: $"Breakfast name must be atleast {Models.Breakfast.MinNameLength} characters long and at most {Models.Breakfast.MaxNameLength} characters long."
+        ); 
+
+        public static Error InvalidDescription => Error.Validation(
+            code: "Breakfast.InvalidDescription",
+            description: $"Breakfast description must be atleast {Models.Breakfast.MinDescriptionLength} characters long and at most {Models.Breakfast.MaxDescriptionLength} characters long."
+        ); 
+
+        public static Error NotFound => Error.NotFound(
+            code: "Breakfast.NotFound",
+            description: "Breakfast not found."
+        ); 
+    }
+}
